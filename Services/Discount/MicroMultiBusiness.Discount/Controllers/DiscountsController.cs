2e8aa293d14bed543b31ac7dbf96aa5f715a0d61
Can Tokhay <cantokhay@gmail.com>
2024-07-18 21:37:04 +0300

    DiscountCoupon Dapper Microservice

---
+++ Services/Discount/MicroMultiBusiness.Discount/Controllers/DiscountsController.cs
@@ -0,0 +1,53 @@
+﻿using MicroMultiBusiness.Discount.DTOs;
+using MicroMultiBusiness.Discount.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroMultiBusiness.Discount.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiscountsController : ControllerBase
+    {
+        private readonly IDiscountService _discountService;
+
+        public DiscountsController(IDiscountService discountService)
+        {
+            _discountService = discountService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> DiscountCouponList()
+        {
+            var coupons = await _discountService.GetAllDiscountCouponsAsync();
+            return Ok(coupons);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetDiscountCouponById(int id)
+        {
+            var coupon = await _discountService.GetByIdDiscountCouponAsync(id);
+            return Ok(coupon);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateDiscountCoupon(CreateDiscountCouponDTO createCouponDTO)
+        {
+            await _discountService.CreateDiscountCouponAsync(createCouponDTO);
+            return Ok("Created Succesfully");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteDiscountCoupon(int id)
+        {
+            await _discountService.DeleteDiscountCouponAsync(id);
+            return Ok("Deleted Succesfully");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateDiscountCoupon(UpdateDiscountCouponDTO updateCouponDTO)
+        {
+            await _discountService.UpdateDiscountCouponAsync(updateCouponDTO);
+            return Ok("Updated Succesfully");
+        }
+    }
+}
