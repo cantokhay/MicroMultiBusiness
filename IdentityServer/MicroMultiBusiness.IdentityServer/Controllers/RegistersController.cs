d20391b445e9c3f26410b06069d4c0d84e862e84
Can Tokhay <cantokhay@gmail.com>
2024-07-19 18:54:03 +0300

    Section9-IdentityServer4 MicroService, EF version tweaks

---
+++ IdentityServer/MicroMultiBusiness.IdentityServer/Controllers/RegistersController.cs
@@ -0,0 +1,42 @@
+﻿using MicroMultiBusiness.IdentityServer.DTOs;
+using MicroMultiBusiness.IdentityServer.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace MicroMultiBusiness.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public RegistersController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UserRegister(UserRegisterDTO userRegisterDTO)
+        {
+            var user = new ApplicationUser()
+            {
+                UserName = userRegisterDTO.Username,
+                Email = userRegisterDTO.Email,
+                Name = userRegisterDTO.Name,
+                LastName = userRegisterDTO.LastName
+            };
+            var result = await _userManager.CreateAsync(user, userRegisterDTO.Password);
+            if (result.Succeeded)
+            {
+                return Ok("Register Successfully");
+            }
+            else
+            {
+                return Ok("There is an error while registering!!!");
+            }
+        }
+    }
+}
---
+++ IdentityServer/MicroMultiBusiness.IdentityServer/DTOs/UserRegisterDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace MicroMultiBusiness.IdentityServer.DTOs
+{
+    public class UserRegisterDTO
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+    }
+}
