f99a3496fd0a89a5252d3520374e4e6ab25072e7
Can Tokhay <cantokhay@gmail.com>
2024-07-22 23:05:42 +0300

    Section15-ProductDetail ProductList Partial Components

---
+++ FrontEnds/MicroMultiBusiness.WebUI/ViewComponents/ProductDetailViewComponent/_ProductDetailInformationComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MicroMultiBusiness.WebUI.ViewComponents.ProductDetailViewComponent
+{
+    public class _ProductDetailInformationComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ FrontEnds/MicroMultiBusiness.WebUI/ViewComponents/ProductListViewComponents/_ProductListSortingShowingComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MicroMultiBusiness.WebUI.ViewComponents.ProductListViewComponents
+{
+    public class _ProductListSortingShowingComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
