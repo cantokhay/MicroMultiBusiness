a327561421cf04e5e76cadd1e18f004e79d85c9c
Can Tokhay <cantokhay@gmail.com>
2024-07-22 21:38:25 +0300

    Section14-UI Partial Views, Default Partial Views

---
+++ FrontEnds/MicroMultiBusiness.WebUI/ViewComponents/DefaultViewComponents/_FeaturedProductDefaultComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MicroMultiBusiness.WebUI.ViewComponents.DefaultViewComponents
+{
+    public class _FeaturedProductDefaultComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
