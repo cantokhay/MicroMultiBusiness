704a3c2f778eff3d67dbb1e45fac7b81ce18e558
Can Tokhay <cantokhay@gmail.com>
2024-07-22 12:08:17 +0300

    Section13-Basket MicroService with Redis and OAuth2.0

---
+++ Services/Basket/MicroMultiBusiness.Basket/LoginServices/ILoginService.cs
@@ -0,0 +1,7 @@
+﻿namespace MicroMultiBusiness.Basket.LoginServices
+{
+    public interface ILoginService
+    {
+        public string GetUserId { get; }
+    }
+}
---
+++ Services/Basket/MicroMultiBusiness.Basket/Services/BasketService.cs
@@ -0,0 +1,31 @@
+﻿using MicroMultiBusiness.Basket.DTOs;
+using MicroMultiBusiness.Basket.Settings;
+using System.Text.Json;
+
+namespace MicroMultiBusiness.Basket.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly RedisService _redisService;
+
+        public BasketService(RedisService redisService)
+        {
+            _redisService = redisService;
+        }
+
+        public async Task DeleteBasket(string userId)
+        {
+            await _redisService.GetDb().KeyDeleteAsync(userId);
+        }
+
+        public async Task<BasketTotalDTO> GetBasket(string userId)
+        {
+            return JsonSerializer.Deserialize<BasketTotalDTO>(await _redisService.GetDb().StringGetAsync(userId));
+        }
+
+        public async Task SaveBasket(BasketTotalDTO basketTotalDTO)
+        {
+            await _redisService.GetDb().StringSetAsync(basketTotalDTO.UserId, JsonSerializer.Serialize(basketTotalDTO));
+        }
+    }
+}
