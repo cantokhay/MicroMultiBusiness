81aab87c0bb3e8f8a5280ff66c14331d3b857f23
Can TOKHAY <cantokhay@outlook.com>
2024-07-29 12:22:48 +0300

    laptop configurations and DB's

---
+++ Services/Cargo/MicroMultiBusiness.Cargo.DataAccessLayer/Migrations/20240729091528_first-on-laptop.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MicroMultiBusiness.Cargo.DataAccessLayer.Migrations
+{
+    public partial class firstonlaptop : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
