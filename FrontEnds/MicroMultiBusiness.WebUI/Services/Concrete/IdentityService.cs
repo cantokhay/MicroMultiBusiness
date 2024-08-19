8234d30593b50127980c055a09d1e7de43a8a99b
Can Tokhay <cantokhay@gmail.com>
2024-08-19 16:52:37 +0300

    Section31 TestController to token requests and responses, just before OCELOT

---
+++ FrontEnds/MicroMultiBusiness.WebUI/Services/Concrete/IdentityService.cs
@@ -0,0 +1,88 @@
+﻿using IdentityModel.Client;
+using MicroMultiBusiness.DTOLayer.IdentityDTOs.LoginDTOs;
+using MicroMultiBusiness.WebUI.Services.Abstract;
+using MicroMultiBusiness.WebUI.Settings;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using System.Security.Claims;
+
+namespace MicroMultiBusiness.WebUI.Services.Concrete
+{
+    public class IdentityService : IIdentityService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ClientSettings _clientSettings; // ClientSettings is a class that we created in the Settings folder
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public IdentityService(HttpClient httpClient, IOptions<ClientSettings> clientSettings, IHttpContextAccessor httpContextAccessor)
+        {
+            _httpClient = httpClient;
+            _clientSettings = clientSettings.Value;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public async Task<bool> SignIn(SignInDTO signInDTO)
+        {
+            var discoveryEndPoint = await _httpClient.GetDiscoveryDocumentAsync(new DiscoveryDocumentRequest
+            {
+                Address = "http://localhost:5001",
+                Policy = new DiscoveryPolicy
+                {
+                    RequireHttps = false
+                }
+            });
+
+            var passwordTokenRequest = new PasswordTokenRequest
+            {
+                ClientId = _clientSettings.MicroMultiBusinessManagerClient.ClientId,
+                ClientSecret = _clientSettings.MicroMultiBusinessManagerClient.ClientSecret,
+                UserName = signInDTO.UserName,
+                Password = signInDTO.Password,
+                Address = discoveryEndPoint.TokenEndpoint
+            };
+
+            var token = await _httpClient.RequestPasswordTokenAsync(passwordTokenRequest);
+
+            var userInfoRequest = new UserInfoRequest
+            {
+                Token = token.AccessToken,
+                Address = discoveryEndPoint.UserInfoEndpoint
+            };
+
+            var userInfo = await _httpClient.GetUserInfoAsync(userInfoRequest);
+
+            ClaimsIdentity claimsIdentity = new ClaimsIdentity(userInfo.Claims, CookieAuthenticationDefaults.AuthenticationScheme, "name", "role");
+
+            ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+
+            var authenticationProperties = new AuthenticationProperties();
+
+            authenticationProperties.StoreTokens(new List<AuthenticationToken>()
+            {
+                new AuthenticationToken
+                {
+                    Name = OpenIdConnectParameterNames.AccessToken,
+                    Value = token.AccessToken
+                },
+                new AuthenticationToken
+                {
+                    Name = OpenIdConnectParameterNames.RefreshToken,
+                    Value = token.RefreshToken
+                },
+                new AuthenticationToken
+                {
+                    Name = OpenIdConnectParameterNames.ExpiresIn,
+                    Value = DateTime.Now.AddSeconds(token.ExpiresIn).ToString()
+                }
+            });
+
+            authenticationProperties.IsPersistent = false;
+
+            await _httpContextAccessor.HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsPrincipal, authenticationProperties);
+
+            return true;
+        }
+    }
+}
--- IdentityServer/MicroMultiBusiness.IdentityServer/Config.cs
+++ IdentityServer/MicroMultiBusiness.IdentityServer/Config.cs
@@ -8,68 +8,68 @@ using System.Collections.Generic;
 
 namespace MicroMultiBusiness.IdentityServer
 {
-	public static class Config
-	{
-		public static IEnumerable<ApiResource> ApiResources => new ApiResource[]
-		{
-			new ApiResource("ResourceCatalog"){Scopes={"CatalogFullPermission","CatalogReadOnly"}},
-			new ApiResource("ResourceDiscount"){Scopes={"DiscountFullPermission"}},
-			new ApiResource("ResourceOrder"){Scopes={"OrderFullPermission"}},
-			new ApiResource("ResourceCargo"){Scopes={"CargoFullPermission"}},
-			new ApiResource("ResourceBasket"){Scopes={"BasketFullPermission"}},
-			new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
-		};
+    public static class Config
+    {
+        public static IEnumerable<ApiResource> ApiResources => new ApiResource[]
+        {
+            new ApiResource("ResourceCatalog"){Scopes={"CatalogFullPermission","CatalogReadOnly"}},
+            new ApiResource("ResourceDiscount"){Scopes={"DiscountFullPermission"}},
+            new ApiResource("ResourceOrder"){Scopes={"OrderFullPermission"}},
+            new ApiResource("ResourceCargo"){Scopes={"CargoFullPermission"}},
+            new ApiResource("ResourceBasket"){Scopes={"BasketFullPermission"}},
+            new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
+        };
 
-		public static IEnumerable<IdentityResource> IdentityResources => new IdentityResource[]
-		{
-			new IdentityResources.OpenId(),
-			new IdentityResources.Email(),
-			new IdentityResources.Profile()
-		};
+        public static IEnumerable<IdentityResource> IdentityResources => new IdentityResource[]
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Email(),
+            new IdentityResources.Profile()
+        };
 
-		public static IEnumerable<ApiScope> ApiScopes => new ApiScope[]
-		{
-			new ApiScope("CatalogFullPermission","Has been full authorized for catalog operations"),
-			new ApiScope("CatalogReadOnly","Has been read only authorized for catalog operations"),
-			new ApiScope("DiscountFullPermission","Has been full authorized for discount operations"),
-			new ApiScope("OrderFullPermission","Has been full authorized for order operations"),
-			new ApiScope("CargoFullPermission","Has been full authorized for cargo operations"),
-			new ApiScope("BasketFullPermission","Has been full authorized for basket operations"),
-			new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
-		};
+        public static IEnumerable<ApiScope> ApiScopes => new ApiScope[]
+        {
+            new ApiScope("CatalogFullPermission","Has been full authorized for catalog operations"),
+            new ApiScope("CatalogReadOnly","Has been read only authorized for catalog operations"),
+            new ApiScope("DiscountFullPermission","Has been full authorized for discount operations"),
+            new ApiScope("OrderFullPermission","Has been full authorized for order operations"),
+            new ApiScope("CargoFullPermission","Has been full authorized for cargo operations"),
+            new ApiScope("BasketFullPermission","Has been full authorized for basket operations"),
+            new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
+        };
 
-		public static IEnumerable<Client> Clients => new Client[]
-		{
-			new Client
-			{
-				ClientId="MicroMultiBusinessVisitorId",
-				ClientName ="MicroMultiBusiness Visitor User",
-				AllowedGrantTypes = GrantTypes.ClientCredentials,
-				ClientSecrets = {new Secret("MicroMultiBusinessSecretKey".Sha256())},
-				AllowedScopes = {"DiscountFullPermission", "CatalogFullPermission", "CatalogReadOnly"}
-			},
-			new Client
-			{
-				ClientId="MicroMultiBusinessManagerId",
-				ClientName="MicroMultiBusiness Manager User",
-				AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
-				ClientSecrets = {new Secret("MicroMultiBusinessSecretKey".Sha256())},
-				AllowedScopes = {"CatalogFullPermission", "CatalogReadOnly", "BasketFullPermission"}
-			},
-			new Client
-			{
-				ClientId="MicroMultiBusinessAdminId",
-				ClientName="MicroMultiBusiness Admin User",
-				AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
-				ClientSecrets = {new Secret("MicroMultiBusinessSecretKey".Sha256())},
-				AllowedScopes = {"CatalogFullPermission", "CatalogReadOnly","DiscountFullPermission", "OrderFullPermission","CargoFullPermission", "BasketFullPermission",
-					IdentityServerConstants.LocalApi.ScopeName,
-					IdentityServerConstants.StandardScopes.Email,
-					IdentityServerConstants.StandardScopes.OpenId,
-					IdentityServerConstants.StandardScopes.Profile
-				},
-				AccessTokenLifetime = 600
-			}
-		};
-	}
+        public static IEnumerable<Client> Clients => new Client[]
+        {
+            new Client
+            {
+                ClientId="MicroMultiBusinessVisitorId",
+                ClientName ="MicroMultiBusiness Visitor User",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets = {new Secret("MicroMultiBusinessSecretKey".Sha256())},
+                AllowedScopes = {"CatalogReadOnly", "CatalogFullPermission"}
+            },
+            new Client
+            {
+                ClientId="MicroMultiBusinessManagerId",
+                ClientName="MicroMultiBusiness Manager User",
+                AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                ClientSecrets = {new Secret("MicroMultiBusinessSecretKey".Sha256())},
+                AllowedScopes = {"CatalogFullPermission", "CatalogReadOnly", "BasketFullPermission"}
+            },
+            new Client
+            {
+                ClientId="MicroMultiBusinessAdminId",
+                ClientName="MicroMultiBusiness Admin User",
+                AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                ClientSecrets = {new Secret("MicroMultiBusinessSecretKey".Sha256())},
+                AllowedScopes = {"CatalogFullPermission", "CatalogReadOnly","DiscountFullPermission", "OrderFullPermission","CargoFullPermission", "BasketFullPermission",
+                    IdentityServerConstants.LocalApi.ScopeName,
+                    IdentityServerConstants.StandardScopes.Email,
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile
+                },
+                AccessTokenLifetime = 600
+            }
+        };
+    }
 }
