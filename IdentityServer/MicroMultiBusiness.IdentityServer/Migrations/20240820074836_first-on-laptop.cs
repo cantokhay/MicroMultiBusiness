126044c00dc55c1c645a4f50b5a2ccafd65cd4af
Can TOKHAY <cantokhay@outlook.com>
2024-08-20 11:35:10 +0300

    Containers and Migrations for LAPTOP, just before OCELOT

---
+++ IdentityServer/MicroMultiBusiness.IdentityServer/Migrations/20240820074836_first-on-laptop.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MicroMultiBusiness.IdentityServer.Migrations
+{
+    public partial class firstonlaptop : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
