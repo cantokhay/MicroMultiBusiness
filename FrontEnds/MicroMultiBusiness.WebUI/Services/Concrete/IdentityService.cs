2615c626caf5eb21d4d80a7ce64c37b4d248ce42
Can Tokhay <cantokhay@gmail.com>
2024-08-21 15:52:50 +0300

    Section33-Service API Settings

--- FrontEnds/MicroMultiBusiness.WebUI/Services/Concrete/IdentityService.cs
+++ FrontEnds/MicroMultiBusiness.WebUI/Services/Concrete/IdentityService.cs
@@ -15,19 +15,74 @@ namespace MicroMultiBusiness.WebUI.Services.Concrete
         private readonly HttpClient _httpClient;
         private readonly ClientSettings _clientSettings; // ClientSettings is a class that we created in the Settings folder
         private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly ServiceAPISettings _serviceAPISettings; // ServiceAPISettings is a class that we created in the Settings folder
 
-        public IdentityService(HttpClient httpClient, IOptions<ClientSettings> clientSettings, IHttpContextAccessor httpContextAccessor)
+        public IdentityService(HttpClient httpClient, IOptions<ClientSettings> clientSettings, IHttpContextAccessor httpContextAccessor, IOptions<ServiceAPISettings> serviceAPISettings)
         {
             _httpClient = httpClient;
             _clientSettings = clientSettings.Value;
             _httpContextAccessor = httpContextAccessor;
+            _serviceAPISettings = serviceAPISettings.Value;
+        }
+
+        public async Task<bool> GetRefreshToken()
+        {
+            var discoveryEndPoint = await _httpClient.GetDiscoveryDocumentAsync(new DiscoveryDocumentRequest
+            {
+                Address = _serviceAPISettings.IdentityServerURL,
+                Policy = new DiscoveryPolicy
+                {
+                    RequireHttps = false
+                }
+            });
+
+            var refreshToken = await _httpContextAccessor.HttpContext.GetTokenAsync(OpenIdConnectParameterNames.RefreshToken);
+
+            RefreshTokenRequest refreshTokenRequest = new RefreshTokenRequest
+            {
+                Address = discoveryEndPoint.TokenEndpoint,
+                ClientId = _clientSettings.MicroMultiBusinessManagerClient.ClientId,
+                ClientSecret = _clientSettings.MicroMultiBusinessManagerClient.ClientSecret,
+                RefreshToken = refreshToken
+            };
+
+            var token = await _httpClient.RequestRefreshTokenAsync(refreshTokenRequest);
+
+            var authenticationTokens = new List<AuthenticationToken>()
+            {
+                new AuthenticationToken
+                {
+                    Name = OpenIdConnectParameterNames.AccessToken,
+                    Value = token.AccessToken
+                },
+                new AuthenticationToken
+                {
+                    Name = OpenIdConnectParameterNames.RefreshToken,
+                    Value = token.RefreshToken
+                },
+                new AuthenticationToken
+                {
+                    Name = OpenIdConnectParameterNames.ExpiresIn,
+                    Value = DateTime.Now.AddSeconds(token.ExpiresIn).ToString()
+                }
+            };
+
+            var result = await _httpContextAccessor.HttpContext.AuthenticateAsync();
+
+            var properties = result.Properties;
+
+            properties.StoreTokens(authenticationTokens);
+
+            await _httpContextAccessor.HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, result.Principal, properties);
+
+            return true;
         }
 
         public async Task<bool> SignIn(SignInDTO signInDTO)
         {
             var discoveryEndPoint = await _httpClient.GetDiscoveryDocumentAsync(new DiscoveryDocumentRequest
             {
-                Address = "http://localhost:5001",
+                Address = _serviceAPISettings.IdentityServerURL,
                 Policy = new DiscoveryPolicy
                 {
                     RequireHttps = false
---
+++ FrontEnds/MicroMultiBusiness.WebUI/Settings/ServiceAPISettings.cs
@@ -0,0 +1,20 @@
+﻿namespace MicroMultiBusiness.WebUI.Settings
+{
+    public class ServiceAPISettings
+    {
+        public string OcelotURL { get; set; }
+        public string IdentityServerURL { get; set; }
+        public ServiceAPI Basket { get; set; }
+        public ServiceAPI Cargo { get; set; }
+        public ServiceAPI Catalog { get; set; }
+        public ServiceAPI Comment { get; set; }
+        public ServiceAPI Discount { get; set; }
+        public ServiceAPI Image { get; set; }
+        public ServiceAPI Order { get; set; }
+        public ServiceAPI Payment { get; set; }
+    }
+    public class ServiceAPI
+    {
+        public string Path { get; set; }
+    }
+}
