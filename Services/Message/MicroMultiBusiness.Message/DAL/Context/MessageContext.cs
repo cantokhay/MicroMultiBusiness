60771d5e9c8d579b6c1addf968a4aeca7674d55a
Can Tokhay <cantokhay@gmail.com>
2024-09-20 14:59:17 +0300

    Post-Migration-MessageDB

---
+++ Services/Message/MicroMultiBusiness.Message/DAL/Context/MessageContext.cs
@@ -0,0 +1,16 @@
+﻿using MicroMultiBusiness.Message.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MicroMultiBusiness.Message.DAL.Context
+{
+    public class MessageContext : DbContext
+    {
+        public MessageContext(DbContextOptions<MessageContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<UserMessage> UserMessages { get; set; }
+
+    }
+}
