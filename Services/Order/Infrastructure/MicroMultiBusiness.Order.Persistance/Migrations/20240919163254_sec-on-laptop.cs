34260aa0b7bd413e7da4617a1f021641160cc46b
Can TOKHAY <cantokhay@outlook.com>
2024-09-20 15:01:29 +0300

    order-migration

---
+++ Services/Order/Infrastructure/MicroMultiBusiness.Order.Persistance/Migrations/20240919163254_sec-on-laptop.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MicroMultiBusiness.Order.Persistance.Migrations
+{
+    public partial class seconlaptop : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
